07c3fc8268c3ec294dd5b173e98c6d775e080ddb
sheikhlimon <sheikhlimon404@gmail.com>
2026-01-19 16:47:05 +0600

    remove tech stack section from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from './components/Hero'
-import TechStack from './components/TechStack'
 import OSS from './components/OSS'
 import Projects from './components/Projects'
 import RecentLogs from './components/RecentLogs'
@@ -10,7 +9,6 @@ export default function Home() {
       <Hero />
       <RecentLogs />
       <OSS />
-      <TechStack />
       <Projects />
     </div>
   )
